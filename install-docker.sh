a3180d51558eed973c355037a8e2f96f5e60defd
arvat <40163253+arvat@users.noreply.github.com>
2023-04-17 21:11:22 +0300

    install docker script

---
+++ install-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Docker for Debian installation script
+# run as root
+
+#install docker
+apt update
+apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt update
+apt-cache policy docker-ce
+apt install docker-ce
+
+# install docker-compose
+curl -L https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
