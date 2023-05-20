330e2995b5c49026acfac5f67366ee37895c6bcb
Vladimir Arvat <40163253+arvat@users.noreply.github.com>
2023-05-20 13:01:41 +0300

    Update install-docker.sh
    autoconfirm install

--- install-docker.sh
+++ install-docker.sh
@@ -4,12 +4,12 @@
 
 #install docker
 apt update
-apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt update
 apt-cache policy docker-ce
-apt install docker-ce
+apt -y install docker-ce
 
 # install docker-compose
 curl -L https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
