e3f4bbcd9debb6cc521d0317cb5a3bb8400ec2d4
Vladimir Arvat <40163253+arvat@users.noreply.github.com>
2023-06-02 17:02:35 +0300

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -5,7 +5,7 @@
 #install docker
 apt update
 apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt update
 apt-cache policy docker-ce
